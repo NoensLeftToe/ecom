87a11be849ec5d4b535d45579d62b7a604f1f161
Sweta <73096700+nazinegi@users.noreply.github.com>
2025-01-16 23:01:54 +0530

    frontend components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './component/layout/Header/Header';
+import Footer from './component/layout/Footer/Footer';
+import Home from './component/Home/Home';
+import './App.css';
+import WebFont from 'webfontloader';
+
+function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"]
+      }
+    });
+  }, []);
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/component/Home/Home.jsx
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react'
+import { CgMouse } from "react-icons/cg";
+import Product from './Product';
+import "./Home.css"
+
+
+const product = {
+  name: "hotarubi mask",
+  images: [{url:"https://i.pinimg.com/736x/ba/a9/63/baa963bfdbda885cf24392b5e347603f.jpg"}],
+  price:"3000$",
+  _id:"abhishek",
+}
+
+const Home = () => {
+  return (
+    <Fragment>
+        <div className="banner">
+            <p><span>Welcome to Power-Ranger.</span></p>
+            <h1> <strong><span>FIND ALL THE MERCH's </span>BELOW</strong></h1>
+
+            <a href="#container">
+                <button>
+                    Scroll <CgMouse />
+                </button>
+            </a>
+        </div>
+        <h2 className="homeHeading">Featured Products</h2>
+        <div className="container" id="container">
+        <Product product={product}/>
+        <Product product={product}/>
+        <Product product={product}/>
+        <Product product={product}/>
+        <Product product={product}/>
+        <Product product={product}/>
+        <Product product={product}/>
+        </div>
+    </Fragment>
+  )
+}
+
+export default Home
