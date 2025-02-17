a234f5fbd15958f12c361a4a1a506335c0d97b57
Sweta <73096700+nazinegi@users.noreply.github.com>
2025-02-17 17:05:40 +0530

    hi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
 import { Provider } from 'react-redux';
 import store from "./store";
 import { ToastContainer, toast } from 'react-toastify'; // For toast notifications
